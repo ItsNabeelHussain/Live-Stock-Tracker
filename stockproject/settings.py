888c8ef8476e34f22c75f5e14b35af03602ef4f2
Nabeel Hussain <93263502+Nabeel-Python-Developer@users.noreply.github.com>
2022-04-04 15:23:11 +0500

    Update settings.py

--- stockproject/settings.py
+++ stockproject/settings.py
@@ -16,8 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f5$ma-6m%t067fdf(3!&1v=mnfjirc1qnpx2)6&14f&kz(!+up'
@@ -150,3 +149,4 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
